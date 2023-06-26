246cf3fafe4a3715a71b6deb4c2ff1a34286c68e
aCuriousDev <quentinmarius@gmail.com>
2023-06-26 05:52:31 +0200

    updated home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,12 +15,32 @@ import {
   Avatar,
 } from "@chakra-ui/react";
 import { PiBracketsCurlyBold } from "react-icons/pi";
-import  profileSrc from "../assets/img/quentin_profile.jpg"
+import profileSrc from "../assets/img/quentin_profile.jpg";
 
 export default function Home() {
   return (
     <>
       <Container>
+        <Box
+          borderRadius="lg"
+          mb={6}
+          p={3}
+          textAlign="center"
+          bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+          css={{ backdropFilter: "blur(10px)" }}
+        >
+          Hello, je suis un{" "}
+          <Tooltip
+            label="in progress..."
+            bg="green.600"
+            placement="top"
+            hasArrow
+            borderRadius={"full"}
+          >
+            développeur
+          </Tooltip>{" "}
+          basé à Lyon en France !
+        </Box>
         <Box display={{ md: "flex" }}>
           <Box flexGrow={1}>
             <Heading as="h2" variant="page-title">
@@ -53,170 +73,6 @@ export default function Home() {
             </Box>
           </Box>
         </Box>
-        <Box
-          borderRadius="lg"
-          mb={6}
-          p={3}
-          textAlign="center"
-          bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
-          css={{ backdropFilter: "blur(10px)" }}
-        >
-          Hello, je suis un{" "}
-          <Tooltip label="in progress..." hasArrow openDelay={500} closeDelay={500}>
-            développeur
-          </Tooltip>{" "}
-          basé à Lyon, France !
-        </Box>
-
-        <Heading>Home Page</Heading>
-        <SimpleGrid spacing={5}>
-          <Card
-            direction={{ base: "column", sm: "row" }}
-            overflow="hidden"
-            variant="outline"
-          >
-            <Image
-              
-              objectFit="cover"
-              maxW={{ base: "100%", sm: "200px" }}
-              src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-              alt="Caffe Latte"
-            />
-
-            <Stack>
-              <CardBody>
-                <Heading size="md">The perfect latte</Heading>
-
-                <Text py="2">
-                  Caffè latte is a coffee beverage of Italian origin made with
-                  espresso and steamed milk.
-                </Text>
-              </CardBody>
-
-              <CardFooter>
-                <Button variant="solid" colorScheme="blue">
-                  Buy Latte
-                </Button>
-              </CardFooter>
-            </Stack>
-          </Card>
-          <Card
-            direction={{ base: "column", sm: "row" }}
-            overflow="hidden"
-            variant="outline"
-          >
-            <Image
-              objectFit="cover"
-              maxW={{ base: "100%", sm: "200px" }}
-              src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-              alt="Caffe Latte"
-            />
-
-            <Stack>
-              <CardBody>
-                <Heading size="md">The perfect latte</Heading>
-
-                <Text py="2">
-                  Caffè latte is a coffee beverage of Italian origin made with
-                  espresso and steamed milk.
-                </Text>
-              </CardBody>
-
-              <CardFooter>
-                <Button variant="solid" colorScheme="blue">
-                  Buy Latte
-                </Button>
-              </CardFooter>
-            </Stack>
-          </Card>
-          <Card
-            direction={{ base: "column", sm: "row" }}
-            overflow="hidden"
-            variant="outline"
-          >
-            <Image
-              objectFit="cover"
-              maxW={{ base: "100%", sm: "200px" }}
-              src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-              alt="Caffe Latte"
-            />
-
-            <Stack>
-              <CardBody>
-                <Heading size="md">The perfect latte</Heading>
-
-                <Text py="2">
-                  Caffè latte is a coffee beverage of Italian origin made with
-                  espresso and steamed milk.
-                </Text>
-              </CardBody>
-
-              <CardFooter>
-                <Button variant="solid" colorScheme="blue">
-                  Buy Latte
-                </Button>
-              </CardFooter>
-            </Stack>
-          </Card>
-          <Card
-            direction={{ base: "column", sm: "row" }}
-            overflow="hidden"
-            variant="outline"
-          >
-            <Image
-              objectFit="cover"
-              maxW={{ base: "100%", sm: "200px" }}
-              src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-              alt="Caffe Latte"
-            />
-
-            <Stack>
-              <CardBody>
-                <Heading size="md">The perfect latte</Heading>
-
-                <Text py="2">
-                  Caffè latte is a coffee beverage of Italian origin made with
-                  espresso and steamed milk.
-                </Text>
-              </CardBody>
-
-              <CardFooter>
-                <Button variant="solid" colorScheme="blue">
-                  Buy Latte
-                </Button>
-              </CardFooter>
-            </Stack>
-          </Card>
-          <Card
-            direction={{ base: "column", sm: "row" }}
-            overflow="hidden"
-            variant="outline"
-          >
-            <Image
-              objectFit="cover"
-              maxW={{ base: "100%", sm: "200px" }}
-              src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-              alt="Caffe Latte"
-            />
-
-            <Stack>
-              <CardBody>
-                <Heading size="md">The perfect latte</Heading>
-
-                <Text py="2">
-                  Caffè latte is a coffee beverage of Italian origin made with
-                  espresso and steamed milk.
-                </Text>
-              </CardBody>
-
-              <CardFooter>
-                <Button variant="solid" colorScheme="blue">
-                  Buy Latte
-                </Button>
-              </CardFooter>
-            </Stack>
-          </Card>
-        </SimpleGrid>
       </Container>
     </>
   );
